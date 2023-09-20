2e844a26ed9fc6334cf32b40c47077d45fc75853
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-09-20 12:09:00 -0400

    prettier

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 const inquirer = require("inquirer");
-const {options, newDeps, newRole, newEmp, updateEmp} = require("./lib/prompts");
-const {departmentList, roleList, employees} = require("./lib/prompts")
+const {
+  options,
+  newDeps,
+  newRole,
+  newEmp,
+  updateEmp,
+  departmentList,
+  roleList,
+  employees,
+} = require("./lib/prompts");
 
 function init() {
   inquirer.prompt(options).then((answers) => {
     if (answers.option === "addDepartment") {
-        inquirer.prompt(newDeps).then((answers) => {
-            departmentList.push(answers.newDepartment)
-            console.log(`Added ${answers.newDepartment}`)
-            init()
-        })
+      inquirer.prompt(newDeps).then((answers) => {
+        departmentList.push(answers.newDepartment);
+        console.log(`Added ${answers.newDepartment}`);
+        init();
+      });
     } else if (answers.option === "addRole") {
-        inquirer.prompt(newRole).then((answers) => {
-            roleList.push(answers.newRoleName)
-            console.log(answers)
-            init()
-        })
+      inquirer.prompt(newRole).then((answers) => {
+        roleList.push(answers.newRoleName);
+        console.log(answers);
+        init();
+      });
     } else if (answers.option === "addEmployee") {
-        inquirer.prompt(newEmp).then((answers) => {
-            employees.push(`${answers.newEmpName} ${answers.newEmpLast}`)
-            console.log(answers)
-            init()
-        })
+      inquirer.prompt(newEmp).then((answers) => {
+        employees.push(`${answers.newEmpName} ${answers.newEmpLast}`);
+        console.log(answers);
+        init();
+      });
     } else if (answers.option === "updateEmployee") {
-        inquirer.prompt(updateEmp).then((answers) => {
-            console.log(answers)
-            init()
-        })
+      inquirer.prompt(updateEmp).then((answers) => {
+        console.log(answers);
+        init();
+      });
     } else if (answers.option === "DEPARTMENTS") {
-        console.log(departmentList)
-        init()
+      console.log(departmentList);
+      init();
     } else if (answers.option === "ROLES") {
-        console.log(roleList)
-        init()
+      console.log(roleList);
+      init();
     } else if (answers.option === "EMPLOYEES") {
-        console.log(employees)
-        init()
+      console.log(employees);
+      init();
     }
   });
 }
-init()
+init();
