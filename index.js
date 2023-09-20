f5dfb87872ffeb992dc673ad79aa9bfb19e4496c
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-09-20 11:52:05 -0400

    added remaining prompts and placeholder logic

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const inquirer = require("inquirer");
-const options = require("./lib/prompts");
+const {options, newDeps, newRole, newEmp, updateEmp} = require("./lib/prompts");
+const {departmentList, roleList, employees} = require("./lib/prompts")
 
 function init() {
   inquirer.prompt(options).then((answers) => {
-    console.log(`You have selected ${answers.option}`);
+    if (answers.option === "addDepartment") {
+        inquirer.prompt(newDeps).then((answers) => {
+            departmentList.push(answers.newDepartment)
+            console.log(`Added ${answers.newDepartment}`)
+            init()
+        })
+    } else if (answers.option === "addRole") {
+        inquirer.prompt(newRole).then((answers) => {
+            roleList.push(answers.newRoleName)
+            console.log(answers)
+            init()
+        })
+    } else if (answers.option === "addEmployee") {
+        inquirer.prompt(newEmp).then((answers) => {
+            employees.push(`${answers.newEmpName} ${answers.newEmpLast}`)
+            console.log(answers)
+            init()
+        })
+    } else if (answers.option === "updateEmployee") {
+        inquirer.prompt(updateEmp).then((answers) => {
+            console.log(answers)
+            init()
+        })
+    } else if (answers.option === "DEPARTMENTS") {
+        console.log(departmentList)
+        init()
+    } else if (answers.option === "ROLES") {
+        console.log(roleList)
+        init()
+    } else if (answers.option === "EMPLOYEES") {
+        console.log(employees)
+        init()
+    }
   });
 }
 init()
